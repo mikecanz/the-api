d2bd23c66276804f963d4dcf2f1c4d2f8ad48918
Michael F. Canzoneri <mikecanzoneri@gmail.com>
2012-09-01 19:10:46 -0400

    Update the restart script with a more full featured set or arguments for logging

--- script/restart-dev.sh
+++ script/restart-dev.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-
-cd /home/ec2-user/the-api/
+   
+cd /home/ec2-user/sg-constable/
 for n in `ps aux | grep plack | grep -v grep  | awk -F' ' '{print $2}'`; do kill $n; done;
-/usr/bin/perl /usr/local/bin/plackup -r -R lib -E development Starman --workers=3 -p 5000 -a bin/app.pl &
+/usr/bin/perl /usr/local/bin/plackup -r -R lib -E development Starman --workers=3 -p 5000 -a bin/app.pl --error-log logs/development.log --access-log logs/access.log 2>&1 &
 sudo /sbin/service nginx restart
