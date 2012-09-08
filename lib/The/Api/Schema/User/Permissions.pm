133071d74f1a19297adca0c951b175eb3d5e8db9
Michael F. Canzoneri <mikecanzoneri@gmail.com>
2012-09-08 00:47:49 -0400

    Tidy up the other Schema files

--- lib/The/Api/Schema/User/Permissions.pm
+++ lib/The/Api/Schema/User/Permissions.pm
@@ -1,28 +1,26 @@
 package The::Api::Schema::User::Permissions;
-  
+
 use Digest::MD5 qw(md5_hex);
-  
+
 use Mongoose::Class;
 with 'Mongoose::EmbeddedDocument';
-  
-has 'admin'                      => (is => 'rw', isa => 'Bool', default => 0);
+
+has 'admin' => ( is => 'rw', isa => 'Bool', default => 0 );
 #has 'some_other_permission' 	 => (is => 'rw', isa => 'Bool', default => 0);
-  
-sub has_permission
-{
+
+sub has_permission {
     my $self = shift;
     my $perm = shift;
-    return ($self->{admin} || $self->{$perm});
+    return ( $self->{admin} || $self->{$perm} );
 }
-  
-sub TO_JSON
-{
-    my ($self, @scope) = @_;
+
+sub TO_JSON {
+    my ( $self, @scope ) = @_;
     my $collapsed = $self->collapse();
     $collapsed->{id} = $self->{_id}->{value};
     delete $collapsed->{_id};
     return $collapsed;
 }
-  
+
 no Moose;
 __PACKAGE__->meta->make_immutable;
